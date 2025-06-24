e24a9555580cd826603ccf8e029d08823ce47cb2
Alex Kharichev <myx@myx.nz>
2025-06-24 19:01:53 +0300

    * ImageSync ssh multiplexing

--- sh-scripts/DistroImageSync.fn.sh
+++ sh-scripts/DistroImageSync.fn.sh
@@ -349,19 +349,19 @@ case "$0" in
 				echo "                Displays all sync tasks for all build stages from all projects in distro." >&2
 				echo "                This command doesn't need project selector and any other arguments." >&2
 				echo >&2
-				echo "    --execute-source-prepare-pull [--no-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "    --execute-source-prepare-pull [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
 				echo "                Execute tasks for source-prapare pull stage (before source-prepare)." >&2
 				echo >&2
-				echo "    --execute-source-process-push [--no-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "    --execute-source-process-push [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
 				echo "                Execute tasks for image-prapare push stage (on image-prepare, after source)." >&2
 				echo >&2
-				echo "    --execute-image-prepare-pull [--no-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "    --execute-image-prepare-pull [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
 				echo "                Execute tasks for image-prapare pull stage (on image-prepare, before deploy)." >&2
 				echo >&2
-				echo "    --execute-image-process-push [--no-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "    --execute-image-process-push [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
 				echo "                Execute tasks for image-prapare push stage (on image-prepare, before deploy)." >&2
 				echo >&2
-				echo "    --execute-image-install-pull [--no-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "    --execute-image-install-pull [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
 				echo "                Execute tasks for image-install pull stage (on image-install, before deploy)." >&2
 				echo >&2
 				echo "    --print-tasks" >&2
@@ -382,6 +382,9 @@ case "$0" in
 				echo "                Takes text columns with '<targetSpec> <sourceSpec> [<branch>]' where <targetSpec>." >&2
 				echo "                is normally a local project full name, <sourceSpec> is git repository URL and <branch>" >&2
 				echo "                is optional branch name ('main' and 'master' are tried is ommited)." >&2
+				echo >&2
+				echo "    --(script|execute)-* [--no-ssh-mx|--one-ssh-mx] [--parallel [N] / --sequence / --portable]" >&2
+				echo "                '--no-ssh-mx|--one-ssh-mx' controls use of ssh multiplexing." >&2
 				echo >&2
 				echo "  Examples:" >&2
 				echo >&2
