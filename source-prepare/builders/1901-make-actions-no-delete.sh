97e01c29d176f3c83d223caa9629b9eaaa2b1072
Alex Kharichev <myx@myx.nz>
2025-07-16 20:42:17 +0300

    * debug support

--- source-prepare/builders/1901-make-actions-no-delete.sh
+++ source-prepare/builders/1901-make-actions-no-delete.sh
@@ -1,5 +1,7 @@
 
 echo "Rebuilding 'source' actions (merge with current actions)..." >&2
 
+[ "full" != "$MDSC_DETAIL" ] || set -x
+
 DistroSystemContext --distro-from-source
 Distro RebuildActions --no-delete
