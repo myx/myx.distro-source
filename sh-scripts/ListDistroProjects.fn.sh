7f3b06e49bf9a9e78feca176d4ab2b70d119e8e7
Alex Kharichev <myx@myx.nz>
2025-09-24 22:20:18 +0300

    * ListAllProjects

--- sh-scripts/ListDistroProjects.fn.sh
+++ sh-scripts/ListDistroProjects.fn.sh
@@ -55,25 +55,22 @@ ListDistroProjects(){
 					set +e ; return 1
 				fi
 
-				if [ "$MDSC_NO_CACHE" = "--no-cache" ] || [ ! -d "$MDSC_CACHED" ] ; then
-					. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/system-context/DistroSystemListAllProjectsNoCache.include"
-					return 0
-				fi
+				if [ "$MDSC_NO_CACHE" != "--no-cache" ] && [ -d "$MDSC_CACHED" ] ; then
+					local buildDate="$MDSC_CACHED/build-time-stamp.txt"
+					if [ -f "$buildDate" ]; then
+						local cacheFile="$MDSC_CACHED/all-project-names.txt"
+						if [ -f "$cacheFile" ] && [ -f "$buildDate" ] && [ ! "$cacheFile" -ot "$buildDate" ] ; then
+							[ -z "$MDSC_DETAIL" ] || echo "| $MDSC_CMD: --all-projects, using cached: all-project-names.txt" >&2
+							cat "$cacheFile"
+							return 0
+						fi
 
-				local buildDate="$MDSC_CACHED/build-time-stamp.txt"
-				if [ -f "$buildDate" ]; then
-					local cacheFile="$MDSC_CACHED/all-project-names.txt"
-					if [ -f "$cacheFile" ] && [ -f "$buildDate" ] && [ ! "$cacheFile" -ot "$buildDate" ] ; then
-						[ -z "$MDSC_DETAIL" ] || echo "| $MDSC_CMD: --all-projects, using cached: all-project-names.txt" >&2
-						cat "$cacheFile"
+						echo "| $MDSC_CMD: --all-projects, caching projects ($MDSC_OPTION)" >&2
+						. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/system-context/DistroSystemListAllProjectsNoCache.include" \
+							| tee "$cacheFile.$$.tmp"
+						mv -f "$cacheFile.$$.tmp" "$cacheFile" || :
 						return 0
 					fi
-
-					echo "| $MDSC_CMD: --all-projects, caching projects ($MDSC_OPTION)" >&2
-					. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/system-context/DistroSystemListAllProjectsNoCache.include" \
-						| tee "$cacheFile.$$.tmp"
-					mv -f "$cacheFile.$$.tmp" "$cacheFile" || :
-					return 0
 				fi
 				
 				. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/system-context/DistroSystemListAllProjectsNoCache.include"
