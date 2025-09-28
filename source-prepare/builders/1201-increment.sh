2b6dcbe819f9c1abab8d5f9d5586b1ffb181627e
Alex Kharichev <myx@myx.nz>
2025-09-28 19:38:10 +0300

    * minor: source code formatting

--- source-prepare/builders/1201-increment.sh
+++ source-prepare/builders/1201-increment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-[ "full" != "$MDSC_DETAIL" ] || set -x
+[ full != "$MDSC_DETAIL" ] || set -x
 
-Require ListDistroProvides
-ListDistroProvides --select-changed --filter-and-cut "source-prepare:increment" \
+Distro ListDistroProvides --select-changed --filter-and-cut "source-prepare:increment" \
 | { grep -e " build.number$" || [ $? -eq 1 ] ; } \
-| cut -d" " -f1 | while read -r projectName ; do
+| cut -d" " -f1 \
+| while read -r projectName ; do
 	CHECK_FILE="$MMDAPP/source/${projectName#$MMDAPP/source/}/build.number"
 	if [ -f "$CHECK_FILE" ] ; then
 		echo "build.number: $projectName: INCREMENT!" >&2
