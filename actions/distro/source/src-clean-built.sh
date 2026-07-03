febd7bcd1fb1caa3ed17beca4db352b9a6bdd744
Alex Kharichev <myx@myx.nz>
2026-07-03 13:38:06 +0400

    ! minor: Action script

--- actions/distro/source/src-clean-built.sh
+++ actions/distro/source/src-clean-built.sh
@@ -2,4 +2,4 @@
 
 [ -d "$MMDAPP/source" ] || ( echo "⛔ ERROR: expecting 'source' directory." >&2 && exit 1 )
 
-"$MDLT_ORIGIN/myx/myx.distro-source/sh-scripts/CleanSourceOutputs.fn.sh"
+"$MDLT_ORIGIN/myx/myx.distro-source/sh-scripts/CleanAllOutputs.fn.sh"
